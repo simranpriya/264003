05f8e63a4d9983ce92864c47832f1e351602250d
Simran Priya <simranmadhupriya@gmail.com>
2021-04-20 18:41:54 +0530

    commit

--- day1.py
+++ day1.py
@@ -1,1 +1,15 @@
 print("simran")
+
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
