beeff7ce34ecf57ffd3c51c9bacefbb766bfbe7c
Simran Priya <simranmadhupriya@gmail.com>
2021-04-20 18:58:41 +0530

    commit

--- day1.py
+++ day1.py
@@ -13,3 +13,32 @@ a = "Hello"
 b = "World"
 c = a + " " + b
 print(c)
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price)) 
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+x = 5
+y = 3
+print(x == y)
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
