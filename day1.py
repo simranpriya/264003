42ee1e291e84e613291878640b01af3de86a6a4a
Simran Priya <simranmadhupriya@gmail.com>
2021-04-20 22:53:27 +0530

    commit

--- day1.py
+++ day1.py
@@ -31,14 +31,3 @@ x = 5
 y = 3
 print(x == y)
 
-thislist = ["apple", "banana", "cherry", "apple", "cherry"]
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-print(thislist[-1])
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[2:5])
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[2:])
---
+++ day2.py
@@ -0,0 +1,11 @@
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
