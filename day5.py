74499f9c302b53c03c11d1c968743b6ad9848bfc
Simran Priya <simranmadhupriya@gmail.com>
2021-04-25 02:39:59 +0530

    commit

---
+++ day5.py
@@ -0,0 +1,11 @@
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry", "apple"}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+print(len(thisset))
+
+thisset = set(("apple", "banana", "cherry")) 
+print(thisset)
