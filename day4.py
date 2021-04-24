8574542084f935b276313bd9a4306ac86003b568
Simran Priya <simranmadhupriya@gmail.com>
2021-04-25 02:32:32 +0530

    commit

---
+++ day4.py
@@ -0,0 +1,17 @@
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
