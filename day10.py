0fcd97e40694fdeb6f36e1632f2e5c83364b2f36
Simran Priya <simranmadhupriya@gmail.com>
2021-04-26 15:28:49 +0530

    commit

---
+++ day10.py
@@ -0,0 +1,29 @@
+cars = ["Ford", "Volvo", "BMW"]
+cars.append("Honda")
+print(cars)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars.remove("Volvo")
+print(cars)
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
