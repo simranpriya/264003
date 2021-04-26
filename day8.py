8a1e71cb03e055673e4dfa3092c4b14f42c9c29c
Simran Priya <simranmadhupriya@gmail.com>
2021-04-27 01:58:34 +0530

    commit

--- day8.py
+++ day8.py
@@ -12,7 +12,7 @@ my_function("Emil", "Refsnes")
 def my_function(country = "Norway"):
   print("I am from " + country)
 
-my_function("Sweden")
+
 my_function("India")
 my_function()
 my_function("Brazil")
