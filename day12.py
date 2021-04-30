1a10b1535097c54ec5bc747ebd7abfa6d8036811
Simran Priya <simranmadhupriya@gmail.com>
2021-05-01 03:19:53 +0530

    commit

---
+++ day12.py
@@ -0,0 +1,11 @@
+s=input()
+if s== "lol" :
+     print("laugh out loud") 
+elif s=="rofl" :
+     print("rolling on the floor laughing")
+elif s=="lmk" :
+     print("let me know")   
+elif s=="smh" :
+     print("shaking my head")      
+else:
+     print("null abbreviation")      
